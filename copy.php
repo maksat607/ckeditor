6ce6a90bd63caea4466753312ff5644c22d1d97d
Maksat Saparbekov <maksatsaparbekov@Maksats-Mac-mini.local>
2018-07-08 18:25:45 +0600

    addding

---
+++ copy.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CK Editor</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.js">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/fontawesome.css">
+	<script src="ckeditor/ckeditor.js" type="text/javascript"></script>
+</head>
+<body>
+   <form action="" method="post" enctype="multipart/fom-data">
+   	<textarea class="ckeditor" name="ckeditor"></textarea>
+   	
+   </form>
+</body>
+</html>
